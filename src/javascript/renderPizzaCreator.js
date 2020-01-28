89e6498abdfbd9974599da97735a8c1807870cc5
Mr-innocence <bellali1019@gmail.com>
2020-01-29 10:49:33 +1100

    change file path

--- src/javascript/renderPizzaCreator.js
+++ src/javascript/renderPizzaCreator.js
@@ -2,17 +2,17 @@ let state = {
     pizzas: [
         {
             size: 'small', 
-            srcImg: 'src/assets/size.png',
+            srcImg: './assets/size.png',
             price: 9.99,
         },
         {
             size: 'medium', 
-            srcImg: 'src/assets/size.png',
+            srcImg: './assets/size.png',
             price: 10.99,
         },
         {
             size: 'large', 
-            srcImg: 'src/assets/size.png',
+            srcImg: './assets/size.png',
             price: 11.99,
         },
     ],
@@ -20,62 +20,62 @@ let state = {
     toppings: [
         {
             name : 'anchovy',
-            srcImg: 'src/assets/toppings/anchovy.svg',
+            srcImg: './assets/toppings/anchovy.svg',
             price: 0.99,
         },
         {
             name : 'bacon',
-            srcImg: 'src/assets/toppings/bacon.svg',
+            srcImg: './assets/toppings/bacon.svg',
             price: 0.99,
         },
         {
             name : 'basil',
-            srcImg: 'src/assets/toppings/basil.svg',
+            srcImg: './assets/toppings/basil.svg',
             price: 0.99,
         },
         {
             name : 'chili',
-            srcImg: 'src/assets/toppings/chili.svg',
+            srcImg: './assets/toppings/chili.svg',
             price: 0.99,
         },
         {
             name : 'mozzarella',
-            srcImg: 'src/assets/toppings/mozzarella.svg',
+            srcImg: './assets/toppings/mozzarella.svg',
             price: 0.99,
         },
         {
             name : 'mushroom',
-            srcImg: 'src/assets/toppings/mushroom.svg',
+            srcImg: './assets/toppings/mushroom.svg',
             price: 0.99,
         },
         {
             name : 'olive',
-            srcImg: 'src/assets/toppings/olive.svg',
+            srcImg: './assets/toppings/olive.svg',
             price: 0.99,
         },
         {
             name : 'onion',
-            srcImg: 'src/assets/toppings/onion.svg',
+            srcImg: './assets/toppings/onion.svg',
             price: 0.99,
         },
         {
             name : 'pepper',
-            srcImg: 'src/assets/toppings/pepper.svg',
+            srcImg: './assets/toppings/pepper.svg',
             price: 0.99,
         },
         {
             name : 'pepperoni',
-            srcImg: 'src/assets/toppings/pepperoni.svg',
+            srcImg: './assets/toppings/pepperoni.svg',
             price: 0.99,
         },
         {
             name : 'sweetcorn',
-            srcImg: 'src/assets/toppings/sweetcorn.svg',
+            srcImg: './assets/toppings/sweetcorn.svg',
             price: 0.99,
         },
         {
             name : 'tomato',
-            srcImg: 'src/assets/toppings/tomato.svg',
+            srcImg: './assets/toppings/tomato.svg',
             price: 0.99,
         }],
     selectedToppingNames: [],
